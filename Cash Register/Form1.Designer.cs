a3f065975a2ed8a2b47b3862a22f95d82040f609
temp <temp>
2017-10-11 13:27:45 -0400

    Add project files.

---
+++ Cash Register/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Cash_Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleColorLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.kebabLabel = new System.Windows.Forms.Label();
+            this.prizeLabel = new System.Windows.Forms.Label();
+            this.kebabTextBox = new System.Windows.Forms.TextBox();
+            this.kebabPrizeLabel = new System.Windows.Forms.Label();
+            this.friesLabel = new System.Windows.Forms.Label();
+            this.friesTextBox = new System.Windows.Forms.TextBox();
+            this.friesPrizeLabel = new System.Windows.Forms.Label();
+            this.drinkLabel = new System.Windows.Forms.Label();
+            this.drinkTextBox = new System.Windows.Forms.TextBox();
+            this.drinkPrizeLabel = new System.Windows.Forms.Label();
+            this.finalPrizeLabel = new System.Windows.Forms.Label();
+            this.myName = new System.Windows.Forms.Label();
+            this.calculateLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.blackLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedTextBox = new System.Windows.Forms.TextBox();
+            this.printButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleColorLabel
+            // 
+            this.titleColorLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.titleColorLabel.Location = new System.Drawing.Point(0, 0);
+            this.titleColorLabel.Name = "titleColorLabel";
+            this.titleColorLabel.Size = new System.Drawing.Size(386, 83);
+            this.titleColorLabel.TabIndex = 0;
+            this.titleColorLabel.Text = " ";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.titleLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.titleLabel.Font = new System.Drawing.Font("Palatino Linotype", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(104, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(167, 63);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Kebab";
+            // 
+            // kebabLabel
+            // 
+            this.kebabLabel.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kebabLabel.Location = new System.Drawing.Point(12, 117);
+            this.kebabLabel.Name = "kebabLabel";
+            this.kebabLabel.Size = new System.Drawing.Size(181, 27);
+            this.kebabLabel.TabIndex = 2;
+            this.kebabLabel.Text = "Number of Kebabs:";
+            // 
+            // prizeLabel
+            // 
+            this.prizeLabel.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prizeLabel.Location = new System.Drawing.Point(274, 94);
+            this.prizeLabel.Name = "prizeLabel";
+            this.prizeLabel.Size = new System.Drawing.Size(89, 23);
+            this.prizeLabel.TabIndex = 3;
+            this.prizeLabel.Text = "Prize each";
+            // 
+            // kebabTextBox
+            // 
+            this.kebabTextBox.Location = new System.Drawing.Point(200, 117);
+            this.kebabTextBox.Name = "kebabTextBox";
+            this.kebabTextBox.Size = new System.Drawing.Size(71, 20);
+            this.kebabTextBox.TabIndex = 4;
+            // 
+            // kebabPrizeLabel
+            // 
+            this.kebabPrizeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kebabPrizeLabel.Location = new System.Drawing.Point(298, 117);
+            this.kebabPrizeLabel.Name = "kebabPrizeLabel";
+            this.kebabPrizeLabel.Size = new System.Drawing.Size(54, 21);
+            this.kebabPrizeLabel.TabIndex = 5;
+            this.kebabPrizeLabel.Text = "$2.49";
+            // 
+            // friesLabel
+            // 
+            this.friesLabel.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.friesLabel.Location = new System.Drawing.Point(12, 144);
+            this.friesLabel.Name = "friesLabel";
+            this.friesLabel.Size = new System.Drawing.Size(173, 27);
+            this.friesLabel.TabIndex = 6;
+            this.friesLabel.Text = "Number of Fries:";
+            // 
+            // friesTextBox
+            // 
+            this.friesTextBox.Location = new System.Drawing.Point(200, 145);
+            this.friesTextBox.Name = "friesTextBox";
+            this.friesTextBox.Size = new System.Drawing.Size(71, 20);
+            this.friesTextBox.TabIndex = 7;
+            // 
+            // friesPrizeLabel
+            // 
+            this.friesPrizeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.friesPrizeLabel.Location = new System.Drawing.Point(298, 144);
+            this.friesPrizeLabel.Name = "friesPrizeLabel";
+            this.friesPrizeLabel.Size = new System.Drawing.Size(54, 21);
+            this.friesPrizeLabel.TabIndex = 8;
+            this.friesPrizeLabel.Text = "$1.89";
+            // 
+            // drinkLabel
+            // 
+            this.drinkLabel.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drinkLabel.Location = new System.Drawing.Point(12, 171);
+            this.drinkLabel.Name = "drinkLabel";
+            this.drinkLabel.Size = new System.Drawing.Size(173, 27);
+            this.drinkLabel.TabIndex = 9;
+            this.drinkLabel.Text = "Number of Drinks:";
+            // 
+            // drinkTextBox
+            // 
+            this.drinkTextBox.Location = new System.Drawing.Point(200, 175);
+            this.drinkTextBox.Name = "drinkTextBox";
+            this.drinkTextBox.Size = new System.Drawing.Size(71, 20);
+            this.drinkTextBox.TabIndex = 10;
+            // 
+            // drinkPrizeLabel
+            // 
+            this.drinkPrizeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drinkPrizeLabel.Location = new System.Drawing.Point(298, 175);
+            this.drinkPrizeLabel.Name = "drinkPrizeLabel";
+            this.drinkPrizeLabel.Size = new System.Drawing.Size(54, 21);
+            this.drinkPrizeLabel.TabIndex = 11;
+            this.drinkPrizeLabel.Text = "$0.99";
+            // 
+            // finalPrizeLabel
+            // 
+            this.finalPrizeLabel.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finalPrizeLabel.Location = new System.Drawing.Point(18, 208);
+            this.finalPrizeLabel.Name = "finalPrizeLabel";
+            this.finalPrizeLabel.Size = new System.Drawing.Size(344, 110);
+            this.finalPrizeLabel.TabIndex = 12;
+            this.finalPrizeLabel.Text = " ";
+            // 
+            // myName
+            // 
+            this.myName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.myName.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.myName.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myName.Location = new System.Drawing.Point(77, 40);
+            this.myName.Name = "myName";
+            this.myName.Size = new System.Drawing.Size(40, 21);
+            this.myName.TabIndex = 13;
+            this.myName.Text = "The";
+            // 
+            // calculateLabel
+            // 
+            this.calculateLabel.Font = new System.Drawing.Font("Prestige Elite Std", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateLabel.Location = new System.Drawing.Point(24, 263);
+            this.calculateLabel.Name = "calculateLabel";
+            this.calculateLabel.Size = new System.Drawing.Size(328, 68);
+            this.calculateLabel.TabIndex = 14;
+            this.calculateLabel.Text = " ";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.calculateButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.calculateButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.Location = new System.Drawing.Point(104, 208);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(166, 36);
+            this.calculateButton.TabIndex = 15;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // blackLabel
+            // 
+            this.blackLabel.BackColor = System.Drawing.Color.Black;
+            this.blackLabel.Location = new System.Drawing.Point(24, 341);
+            this.blackLabel.Name = "blackLabel";
+            this.blackLabel.Size = new System.Drawing.Size(328, 12);
+            this.blackLabel.TabIndex = 16;
+            this.blackLabel.Text = " ";
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.Font = new System.Drawing.Font("Palatino Linotype", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.Location = new System.Drawing.Point(22, 365);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(134, 25);
+            this.tenderedLabel.TabIndex = 17;
+            this.tenderedLabel.Text = "Tendered:";
+            // 
+            // tenderedTextBox
+            // 
+            this.tenderedTextBox.Location = new System.Drawing.Point(132, 373);
+            this.tenderedTextBox.Name = "tenderedTextBox";
+            this.tenderedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.tenderedTextBox.TabIndex = 18;
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(82, 413);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(206, 23);
+            this.printButton.TabIndex = 19;
+            this.printButton.Text = "Print Receipt";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(740, 448);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.tenderedTextBox);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.blackLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.calculateLabel);
+            this.Controls.Add(this.myName);
+            this.Controls.Add(this.finalPrizeLabel);
+            this.Controls.Add(this.drinkPrizeLabel);
+            this.Controls.Add(this.drinkTextBox);
+            this.Controls.Add(this.drinkLabel);
+            this.Controls.Add(this.friesPrizeLabel);
+            this.Controls.Add(this.friesTextBox);
+            this.Controls.Add(this.friesLabel);
+            this.Controls.Add(this.kebabPrizeLabel);
+            this.Controls.Add(this.kebabTextBox);
+            this.Controls.Add(this.prizeLabel);
+            this.Controls.Add(this.kebabLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.titleColorLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleColorLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label kebabLabel;
+        private System.Windows.Forms.Label prizeLabel;
+        private System.Windows.Forms.TextBox kebabTextBox;
+        private System.Windows.Forms.Label kebabPrizeLabel;
+        private System.Windows.Forms.Label friesLabel;
+        private System.Windows.Forms.TextBox friesTextBox;
+        private System.Windows.Forms.Label friesPrizeLabel;
+        private System.Windows.Forms.Label drinkLabel;
+        private System.Windows.Forms.TextBox drinkTextBox;
+        private System.Windows.Forms.Label drinkPrizeLabel;
+        private System.Windows.Forms.Label finalPrizeLabel;
+        private System.Windows.Forms.Label myName;
+        private System.Windows.Forms.Label calculateLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label blackLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderedTextBox;
+        private System.Windows.Forms.Button printButton;
+    }
+}
+
---
+++ Cash Register/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Cash_Register
+{
+    
+    public partial class Form1 : Form
+    {
+        //global constants
+        const double KEBAB = 2.49;
+        const double FRIES = 1.89;
+        const double DRINK = 0.99;
+        const double TAX = 0.13;
+
+        //global variables
+        int numOfKebabs = 0;
+        int numOfFries = 0;
+        int numOfDrinks = 0;
+        double subTotal = 0;
+        double total = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //double kebabTextBox.Text;
+            numOfKebabs = Convert.ToInt16(kebabTextBox.Text);
+            numOfFries = Convert.ToInt16(friesTextBox.Text);
+            numOfDrinks = Convert.ToInt16(drinkTextBox.Text);
+
+            //calculation
+            double subTotal = numOfKebabs * KEBAB + numOfFries * FRIES + numOfDrinks * DRINK;
+            double tax = subTotal * TAX;
+            double total = subTotal * TAX + subTotal;
+
+            calculateLabel.Text = "Sub Total                 " + subTotal.ToString("C");
+            calculateLabel.Text += "\nTax                       " + tax.ToString("C");
+            calculateLabel.Text += "\nTotal                     " + total.ToString("C");
+        }
+
+        private void printButton_Click(object sender, EventArgs e)
+        {
+
+
+            //formGraphics.Clear(Color.Black);
+
+            //prizeLabel.Visible = false;
+
+            //titleColorLabel.Visible = false;
+            //titleLabel.Visible = false;
+           // myName.Visible = false;
+
+            //kebabLabel.Visible = false;
+            //kebabPrizeLabel.Visible = false;
+            //kebabTextBox.Visible = false;
+
+            //drinkLabel.Visible = false;
+            //drinkPrizeLabel.Visible = false;
+            //drinkTextBox.Visible = false;
+
+            //friesLabel.Visible = false;
+            //friesPrizeLabel.Visible = false;
+            //friesTextBox.Visible = false;
+
+            //calculateButton.Visible = false;
+            //calculateLabel.Visible = false;
+
+            //blackLabel.Visible = false;
+
+            //tenderedLabel.Visible = false;
+            //tenderedTextBox.Visible = false;
+
+            //printButton.Visible = false;
+
+            Refresh();
+
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            formGraphics.FillRectangle(drawBrush, 400, 30, 320, 400);
+
+            Font drawFont = new Font("Palatino Linotype", 14, FontStyle.Italic);
+            drawBrush.Color = Color.Black;
+            //formGraphics.TranslateTransform(430, 40);
+            formGraphics.DrawString("The", drawFont, drawBrush, 480, 42);
+
+            drawFont = new Font("Palatino Linotype", 20, FontStyle.Italic);
+            formGraphics.DrawString("Kebab", drawFont, drawBrush, 505, 40);
+
+
+        }
+    }
+}
